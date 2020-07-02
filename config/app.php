1a35df2da12b527f4a2afb669edb16436ced54ca
aamei33 <786873450@qq.com>
2020-07-02 13:45:49 +0800

    生成用户认证代码

--- config/app.php
+++ config/app.php
@@ -164,7 +164,12 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+
+        //liuyamei 替换语言包类
+        //Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
+        //yamei
+
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
