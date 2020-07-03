10487575af5c9da0952525abb843932920c9c2c8
aamei33 <786873450@qq.com>
2020-07-03 13:13:11 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,7 @@ function route_class()
     return str_replace('.', '-', Route::currentRouteName());
     //此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制。
 }
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
