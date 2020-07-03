a18383ce5092a214ffe881ecf3b3d870049b2ffb
aamei33 <786873450@qq.com>
2020-07-03 13:43:09 +0800

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,4 +25,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
     ];
+
+    public function  topics(){
+        return  $this->hasMany(Topic::class);
+    }
 }
