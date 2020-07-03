1dd05be1385144e05d54acb17d7f6c3969f23200
aamei33 <786873450@qq.com>
2020-07-03 11:31:32 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -9,4 +9,15 @@ class Topic extends Model
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
     ];
+
+    public function category(){
+        //一个帖子数据一个分类
+        return  $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        //一个帖子属于一个用户
+        return $this->belongsTo(User::class);
+    }
+
 }
